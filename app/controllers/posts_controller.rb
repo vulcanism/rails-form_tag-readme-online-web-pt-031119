948ffe9f301ac02816dac2e4cc23429d167d06fe
Savannah C <sgcorrell@gmail.com>
2019-07-23 16:47:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,8 @@ class PostsController < ApplicationController
   end
   
   def create
-  Post.create(title: params[:post][:title], description: params[:post][:description])
-  redirect_to posts_path
-end
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
   
 end
